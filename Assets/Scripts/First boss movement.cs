87147e2724de5d5b1f12abbfcc38134778ff474a
Jonathan Hickerson <tjohoski3165@student.pps.net>
2026-04-21 15:33:10 -0700

    feat: worked on boss movement

--- Assets/Scripts/First boss movement.cs
+++ Assets/Scripts/First boss movement.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
 
@@ -11,12 +13,12 @@ public class Firstbossmovement : MonoBehaviour
     private int[] directionsArr = {-1,1}; //Right = 1 | Left = -1
     private int direction;
     private float currentSpeed; //SE
-    private float maxSpeed; //SE
+    private float maxSpeed = 10; //SE
     private float distanceToDestination; //total distance to cover
     private int maxTravellDistance = 30; //SE
     private float distanceTravelled; //SE 
     private float distanceToNext; //Distance to next waypoint on the track
-    private float acceleration = 5; 
+    private float acceleration = 10; 
     [SerializeField] GameObject nextWaypoint; //nearest waypoint in direction
     [SerializeField] GameObject[] waypoints;
     private Rigidbody2D rb;
@@ -26,37 +28,50 @@ public class Firstbossmovement : MonoBehaviour
 
     void Awake()
     {
+        GenerateDestination();
         rb = GetComponent<Rigidbody2D>();
+        nextWaypoint = waypoints[UnityEngine.Random.Range(0,waypoints.Length)];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (InputManager.JumpWasPressed)
-        {
-            MoveToDestination();
-        }
+        Debug.Log(direction);
+        Debug.Log(Array.IndexOf(waypoints,nextWaypoint));
+        MoveToDestination();
+        UpdateValues();
     }
 
     public void GenerateDestination()
     {
-        direction = directionsArr[Random.Range(0,directionsArr.Length)];
-        distanceToDestination = Random.Range(0,30) * direction;
+        direction = directionsArr[UnityEngine.Random.Range(0,directionsArr.Length)];
+        distanceToDestination = UnityEngine.Random.Range(0,30) * direction;
     }   
 
     private void MoveToDestination()
-    {
-        nextWaypoint = waypoints[Random.Range(0,waypoints.Length)];
-        destination = nextWaypoint.transform.position;
-        currentSpeed = Mathf.Lerp( currentSpeed, maxSpeed, acceleration * Time.deltaTime);
-        rb.velocity = new Vector2(currentSpeed, 0f);
+    {  
+        destination = (nextWaypoint.transform.position - transform.position).normalized;
+        Vector2 targetVelocity = destination * maxSpeed;
+        rb.velocity = Vector2.Lerp(rb.velocity, targetVelocity, acceleration * Time.deltaTime);
+        if(distanceToNext < 0.01)
+        {
+            rb.velocity = new Vector2(0,0);
+            int currentIndex = Array.IndexOf(waypoints, nextWaypoint);
+            nextWaypoint = waypoints[ currentIndex + ( 1 *direction)];
+
+        }
 
 
     }
 
+    private void UpdateValues()
+    {
+        distanceToNext = Vector2.Distance(transform.position, nextWaypoint.transform.position);
+    }
+
     // private IEnumerator MoveToPoint(Vector2 position, Vector2 destination, float speed)
-    // {
+    //  {
         
         
-    // }
+    //  }
 }
