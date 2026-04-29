366e20ad17e9017b541df04f5792ae41a4283166
Eve <zezetta3031@student.pps.net>
2026-04-29 14:20:31 -0700

    fix: update level 1 scene name in main menu

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -5,6 +5,6 @@ public class StartButton : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Big Combinded Scene");
+        SceneManager.LoadScene("Level 1");
     }
 }
