6459c40f320fd2846fe2590a3541dfb0c34f102f
Eve <zezetta3031@student.pps.net>
2026-05-21 14:51:54 -0700

    chore: remove dead code

--- Assets/Scripts/Combat/ShotgunBulletScript.cs
+++ Assets/Scripts/Combat/ShotgunBulletScript.cs
@@ -66,25 +66,7 @@ namespace Combat
         {
             if (other.CompareTag("Dialogue Trigger") || other.gameObject.name.Contains("Bullet") || other.CompareTag("Enemy Boundary"))
                 return;
-
-            if (other.CompareTag("Enemy"))
-            {
-                // EnemyScript enemy = other.GetComponent<EnemyScript>();
-                // if (enemy != null)
-                // {
-                //     enemy.Inflict(0.25);
-                //     HealthScript health = other.GetComponent<HealthScript>();
-                //     if (health != null)
-                //         health.EnemyKilled();
-                // }
-                // else
-                // {
-                //     Debug.Log("No enemy script found on object tagged as enemy");
-                // }
-                Destroy(gameObject);
-                return;
-            }
-
+            
             if (other.CompareTag("Player"))
             {
                 HealthScript health = other.GetComponent<HealthScript>();
